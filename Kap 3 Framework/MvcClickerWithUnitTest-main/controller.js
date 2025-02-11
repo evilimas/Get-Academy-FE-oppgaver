96662f0516af53eca289ad5f88fd3fd0a6d21266
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-02-11 13:06:16 +0100

    update

--- Kap 3 Framework/MvcClickerWithUnitTest-main/controller.js
+++ Kap 3 Framework/MvcClickerWithUnitTest-main/controller.js
@@ -2,6 +2,7 @@
 function doClick() {
     points += pointsPerClick;
     smileyIndex = 1 - smileyIndex;
+    app.replaceChildren()
     updateView();
 }
 
@@ -9,5 +10,11 @@ function buyUpgrade() {
     if (points < 10) return;
     points -= 10;
     pointsPerClick++;
+    app.replaceChildren()
     updateView()
+}
+
+function appendDiv(text, element){
+    element.textContent = text
+    app.appendChild(element)
 }
