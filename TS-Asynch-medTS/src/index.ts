c4cb148a77e5180d2967dedc772ecddd6092beca
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-03-11 13:44:39 +0100

    fix

--- TS-Asynch-medTS/src/index.ts
+++ TS-Asynch-medTS/src/index.ts
@@ -1,36 +1,38 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const app = document.getElementById('app')
+const app: HTMLElement | null = document.getElementById('app');
 
-        getJokes();
-        async function getJokes() : Promise<void> {
+getJokes();
+async function getJokes(): Promise<void> {
+  try {
+    const url = 'https://api.chucknorris.io/jokes/random';
+    const jokeObject = await axios.get(url);
+    const joke = jokeObject.data.value.toLowerCase().split(' ');
+    const jokeWord: string = joke[0];
+    console.log('Word for search is', jokeWord);
+    app!.innerHTML = `<p class="bold">Word for search is ${jokeWord}</p>`;
 
-            try {
-                const url = 'https://api.chucknorris.io/jokes/random';
-                const jokeObject = await axios.get(url);
-                const joke = jokeObject.data.value.toLowerCase().split(" ")
-                const jokeWord = joke[0]
-                console.log('Word for search is', jokeWord);
+    const jokesWithWordUrl: string = `https://api.chucknorris.io/jokes/search?query=${jokeWord}`;
+    const jokesWithWordObjects = await axios.get(jokesWithWordUrl);
 
-                const jokesWithWordUrl = `https://api.chucknorris.io/jokes/search?query=${jokeWord}`
-                const jokesWithWordObjects = await axios.get(jokesWithWordUrl)
-
-                const first25Jokes = jokesWithWordObjects.data.result.slice(0,20)
-                for(let joke of first25Jokes){
-                    // console.log(joke.value)
-                    app!.innerHTML += `
+    const first25Jokes = jokesWithWordObjects.data.result.slice(0, 20);
+    for (let joke of first25Jokes) {
+      app!.innerHTML += `
                     <div>
                         <img src="${joke.icon_url}">
                         <p>${joke.value}<p>
-                    </div>`
-                }
-                console.log(jokesWithWordObjects.data.result)
+                        <hr>
+                    </div>`;
+    }
+    console.log(jokesWithWordObjects.data.result);
+  } catch (error) {
+    app!.innerHTML += `
+        <div>
+            <p>${error}</p>
+            <p>Most likely the word of search was to short, try again</p>
+        </div>
+    `;
 
-
-            }
-            catch (error) {
-                
-                app!.innerHTML += `<p>${error}</p>`
-                console.log('Error: ', error);
-            }
-        }
+    console.log('Error: ', error);
+  }
+}
