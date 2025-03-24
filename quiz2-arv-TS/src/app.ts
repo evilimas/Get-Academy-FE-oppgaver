5c68d3f296b659b513d94a82bcfd38cc685aea44
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-03-24 11:19:07 +0100

    beginned

--- quiz2-arv-TS/src/app.ts
+++ quiz2-arv-TS/src/app.ts
@@ -1,9 +1,14 @@
-import { QuizComponent } from "./components/quiz";
-import { SingleChoiceQuestionComponent } from "./components/singleChoiceQuestion";
-import { MultipleChoiceQuestionComponent } from "./components/multipleChoiceQuestion";
-import { TextQuestionComponent } from "./components/textQuestion";
+import { QuizComponent } from './components/quiz';
+import { SingleChoiceQuestionComponent } from './components/singleChoiceQuestion';
+import { MultipleChoiceQuestionComponent } from './components/multipleChoiceQuestion';
+import { TextQuestionComponent } from './components/textQuestion';
+import { OptionsChoiceQuestionComponent } from './components/optionsChoiceQuestion';
 
-customElements.define("quiz-container", QuizComponent);
-customElements.define("single-choice-question", SingleChoiceQuestionComponent);
-customElements.define("multiple-choice-question", MultipleChoiceQuestionComponent);
-customElements.define("text-question", TextQuestionComponent);
+customElements.define('quiz-container', QuizComponent);
+customElements.define('single-choice-question', SingleChoiceQuestionComponent);
+customElements.define(
+  'multiple-choice-question',
+  MultipleChoiceQuestionComponent
+);
+customElements.define('text-question', TextQuestionComponent);
+customElements.define('options-questions', OptionsChoiceQuestionComponent);
--- quiz2-arv-TS/src/components/optionsChoiceQuestion.ts
+++ quiz2-arv-TS/src/components/optionsChoiceQuestion.ts
@@ -1,3 +1,28 @@
 import { ChoiceQuestionComponent } from './choiceQuestion';
 
-export class OptionsChoiceQuestionComponent extends ChoiceQuestionComponent {}
+export class OptionsChoiceQuestionComponent extends ChoiceQuestionComponent {
+  private answer: Set<string> = new Set();
+
+  protected createInputMarkup(option: string): string {
+    return `
+    <select>
+        <option></option>
+        <option></option>
+    </select>`;
+  }
+
+  protected addEventListeners() {
+    const inputs = this.shadowRoot!.querySelectorAll('input');
+    for (let input of inputs) {
+      input.addEventListener('change', (event: Event) => {
+        const inputElement = event.target as HTMLInputElement;
+        const isChecked = inputElement.checked;
+        const txt = inputElement.parentElement?.textContent;
+        if (isChecked) this.answer.add(txt!);
+        else this.answer.delete(txt!);
+        const answer = Array.from(this.answer).join(',');
+        this.dispatchAnswerChangedEvent(answer);
+      });
+    }
+  }
+}
